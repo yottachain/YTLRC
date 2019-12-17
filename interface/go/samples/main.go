a42430bfed78010f8bd7e9bdb285b29f1a178a07
zhp36 <zhp771202@sina.com>
2019-12-17 01:00:35 -0800

    update sample source code

--- interface/go/samples/main.go
+++ interface/go/samples/main.go
@@ -3,6 +3,7 @@ import "lrcpkg"
 import "fmt"
 import "os"
 import "io/ioutil"
+import "strconv"
 func checkFileIsExist(filename string) bool {
     var exist = true
     if _, err := os.Stat(filename); os.IsNotExist(err) {
@@ -18,83 +19,54 @@ func check(e error) {
 func main(){
    var lrc lrcpkg.LRCEngine
    var lrctype lrcpkg.Shardsinfo
-   lrctype.Handle=-1
-   lrctype.Status=0
-   lrctype.RecoverNum = 34
-   lrctype.RebuilderNum = 3
-   lrctype.OriginalCount =110
-   lrctype.Lostindex = 6
-   lrctype.ShardSize = 222
+   lrctype.Handle=-1    //handle,do not cares the init value
+   lrctype.Status=0     // repaired data status
+   lrctype.RecoverNum = 34   // number of recover shrads 
+   lrctype.RebuilderNum = 3  
+   lrctype.OriginalCount =110 // number of data shards
+   lrctype.Lostindex = 6      // the index of lost shard
+   lrctype.ShardSize = 222    //do not cares the init value
    lrc=lrctype
    a:=lrc.GetRCHandle(&lrctype)
-   lrc.GetNeededShardList(a)
-//   lrc.FreeHandle(&lrctype)
-   f0,_ := os.Open("/root/LRC/test0.dat")
-   t0,_:=ioutil.ReadAll(f0)
-   f0.Close()
-   a0:=lrc.AddShardData(a,t0)
-   if (a0 > 0 || a0 == 0) {
-      fmt.Println("add data test0.dat ok")
-   } 
-   f1,_ := os.Open("/root/LRC/test1.dat")
-   t1,_:=ioutil.ReadAll(f1)
-   f1.Close()
-   a1:=lrc.AddShardData(a,t1)
-   if (a1 > 0 || a1 == 0) {
-      fmt.Println("add data test1.dat ok")
-   } 
-   f2,_ := os.Open("/root/LRC/test2.dat")
-   t2,_:=ioutil.ReadAll(f2)
-   f2.Close()
-   a2:=lrc.AddShardData(a,t2)
-   
-   if (a2 > 0 || a2 == 0) {
-      fmt.Println("add data test2.dat ok")
-   } 
-   f3,_:= os.Open("/root/LRC/test3.dat")
-   t3,_:=ioutil.ReadAll(f3)
-   f3.Close()
-   a3:=lrc.AddShardData(a,t3)
-   if (a3 > 0 || a3 == 0) {
-      fmt.Println("add data test3.dat ok")
-   } 
-   f4,_:= os.Open("/root/LRC/test4.dat")
-   t4,_:=ioutil.ReadAll(f4)
-   f4.Close()
-   a4:=lrc.AddShardData(a,t4)
-   if (a4 > 0 || a4 == 0) {
-      fmt.Println("add data test4.dat ok")
-   } 
+   slist,s := lrc.GetNeededShardList(a)
+   if (s < 0){
+     fmt.Println("something wrong")
+     lrc.FreeHandle(&lrctype)
+     return
+   }
+   if (s == 0){
+     fmt.Println("can not repair the data")
+     lrc.FreeHandle(&lrctype)
+     return
+   }
+   fmt.Println(s)
 
-   f5,_:= os.Open("/root/LRC/test5.dat")
-   t5,_:=ioutil.ReadAll(f5)
-   f5.Close()
-   a5:=lrc.AddShardData(a,t5)
-   
-   if (a5 > 0 || a5 == 0) {
-      fmt.Println("add data test5.dat ok")
-   } 
-   f7,_ := os.Open("/root/LRC/test7.dat")
-   t7,_:=ioutil.ReadAll(f7)
-   f7.Close()
-   a7:=lrc.AddShardData(a,t7)
-   if (a7 > 0 || a7 == 0) {
-      fmt.Println("add data test7.dat ok")
-   } 
-
-   f110,_ := os.Open("/root/LRC/test110.dat")
-   t110,_:=ioutil.ReadAll(f110)
-   f110.Close()
-   a110:=lrc.AddShardData(a,t110)
-   if (a110 > 0 || a110 == 0) {
-      fmt.Println("add data test110.dat ok")
-   } 
+   for e:=slist.Front();nil != e;e=e.Next() {
+       f,_ := os.Open("/root/LRC/test" + strconv.Itoa(int(e.Value.(int16))) + ".dat")
+       t,_:=ioutil.ReadAll(f)
+       f.Close()
+       z:=lrc.AddShardData(a,t)
+       if (z == 0) {
+          fmt.Println("add data test" + strconv.Itoa(int(e.Value.(int16))) + ".dat ok")
+       }
+       if (z < 0){
+          fmt.Println("add data test" + strconv.Itoa(int(e.Value.(int16))) + ".dat error")
+       } 
+       if (z > 0){
+          fmt.Println("add data test" + strconv.Itoa(int(e.Value.(int16))) + ".dat ok")
+          fmt.Println("shard has been repaired!!!")
+          break
+       }
+   }
+   if z < 0 {
+      fmt.Println("Can not rebuild data!!!")
+      return
+   }
    g,k := lrc.GetRebuildData(&lrctype)
    if k > 0 {
      fmt.Println("Rebuild data ok")
    }else {
      fmt.Println("Rebuild data failed")
-
    }
    lrc.FreeHandle(&lrctype)
    fmt.Println(g[0])
