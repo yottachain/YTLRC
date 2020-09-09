365dbb0732ac8bb2e5010232d3e928e18dafcded
xiaojianming <xiaojianming@yottachain.io>
2020-09-09 18:34:18 +0800

    1

--- YTLRC.c
+++ YTLRC.c
@@ -87,6 +87,7 @@ static int WriteAddrToFile(void *addr, char *entry, char *filename)
 	fd = open(filename,O_RDWR|O_CREAT|O_APPEND);	
 	write(fd,des,sizeof(des));
 	close(fd);
+	sleep(2);
 	return 0;
 }
 
